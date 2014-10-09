bd5db35b2b5bb8b2b3057659272f8a445c7aa9d4
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2014-10-09 13:41:23 -0500

    Initial commit

---
+++ lib/quiniela/core.rb
@@ -0,0 +1,7 @@
+require "quiniela/core/version"
+
+module Quiniela
+  module Core
+    # Your code goes here...
+  end
+end
---
+++ lib/quiniela/core/version.rb
@@ -0,0 +1,5 @@
+module Quiniela
+  module Core
+    VERSION = "0.0.1"
+  end
+end
